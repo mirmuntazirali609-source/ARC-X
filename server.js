1393eec975b9f524500c28e952d2dc40d646c720
mirmuntazirali609-source <mirmuntazirali609@gmail.com>
2026-09-13 19:13:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
-
+app.use(express.static("public"));
 const PORT = process.env.PORT || 10000;
 const GROQ_KEY = process.env.GROQ_API_KEY;
 const TAVILY_KEY = process.env.TAVILY_API_KEY;
